9dc15c1bccedbe50612d595f61bd119c3588bc8f
bathas <siva.bathala@dell.com>
2018-11-30 13:47:31 -0700

    autiwiring existing datasource into jdbcrepository for time entries

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 
+import javax.sql.DataSource;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.util.List;
@@ -16,7 +17,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
-    public JdbcTimeEntryRepository(MysqlDataSource dataSource)
+    public JdbcTimeEntryRepository(DataSource dataSource)
     {
       this.jdbcTemplate= new JdbcTemplate(dataSource);
     }
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,28 +4,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.mysql.cj.jdbc.MysqlDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
-    @Value("${SPRING_DATASOURCE_URL}")
-    private String databaseUrl;
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
-    @Bean
-    public TimeEntryRepository timeEntryRepository()
-    {
-        MysqlDataSource mysqlDataSource =  new MysqlDataSource();
-        mysqlDataSource.setURL(databaseUrl);
 
-        return new JdbcTimeEntryRepository(mysqlDataSource);
+    @Bean
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource)
+    {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
